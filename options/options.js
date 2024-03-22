405140df39eb0927c68b4eec25ae32e37867fd8f
Crowdbotics <team@crowdbotics.com>
2024-03-22 16:19:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skincare-inventory--47247",
+  url: "https://skincare-inventory--47247.botics.co",
+  api: "https://skincare-inventory--47247.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
